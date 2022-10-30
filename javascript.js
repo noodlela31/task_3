2db7f605e3e933220d1787808fcc99cd57fd77d2
Mila Oktaviani <115177300+noodlela31@users.noreply.github.com>
2022-10-30 19:39:23 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,15 +3,15 @@ let changeText = document.querySelector("#kalimat");
 changeText.innerText = "Input Teks Baru";
 
 //NO.2
-let newElement = document.createElement('p');
+let newElement = document.createElement("p");
 newElement.innerHTML = "Ini Inputan Teks Baru";
 changeText.appendChild(newElement);
 
 //NO.3
-let newParagraph = document.querySelector('.press');
+let newParagraph = document.querySelector(".press");
 document.body.appendChild(newParagraph);
-let count = document.getElementById('increment');
+let count = document.getElementById("increment");
 
-count.addEventListener('click', function (event) {
-    document.querySelector('#count').innerText++;
+count.addEventListener("click", function (event) {
+    document.querySelector("#count").innerText++;
 });
